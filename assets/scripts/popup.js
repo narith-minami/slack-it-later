1cadd44ac472a23ff52cce95a37bbf9da5828ef0
narith-minami <narith.for.work@gmail.com>
2018-11-15 13:13:37 +0900

    コメントをつけるように修正

--- assets/scripts/popup.js
+++ assets/scripts/popup.js
@@ -1,24 +1,44 @@
 $(function() {
-    chrome.tabs.getSelected(null, function(tab) {
-        var requestURL = localStorage.getItem('request_url');
-        var channel    = localStorage.getItem('channel');
-        var username   = localStorage.getItem('username');
-        var text       = tab.title + ' : ' + tab.url;
 
-        $.ajax({
-            url: requestURL,
-            type: 'post',
-            data: 'payload=' + JSON.stringify({
-                "channel": channel,
-                "username": username,
-                "text": text
-            }),
-            success: function() {
-                console.log('success');
-            },
-            error: function() {
-                console.log('fail');
-            }
-        });
+  let requestURL;
+  let channel;
+  let username;
+  let url;
+  chrome.tabs.getSelected(null, function(tab) {
+      requestURL = localStorage.getItem('request_url');
+      channel    = localStorage.getItem('channel');
+      username   = localStorage.getItem('username');
+      url        = tab.url;
+  });
+
+    $('#post-to-share-button').on('click', function(e) {
+        postToURLAndCommnet();
     });
+
+    function postToURLAndCommnet() {
+          const text       = $('#post-to-slack-comment').val()+' '+ url; //tab.title + ' : ' + tab.url;
+
+          if (!requestURL || requestURL === '') {
+            alert('request URLが設定されていません。');
+            return;
+          }
+
+          $.ajax({
+              url: requestURL,
+              type: 'post',
+              data: 'payload=' + JSON.stringify({
+                  "channel": channel,
+                  "username": username,
+                  "text": text
+              }),
+              success: function() {
+                  console.log('success');
+                  $('#post-to-slack-message').show();
+              },
+              error: function() {
+                  console.log('fail');
+              }
+          });
+    }
+
 });
